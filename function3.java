9fff223e21c6d5f17d3fd616cca6615f96ea21d3
Sowrav <souravpoojary50@gmail.com>
2025-12-01 23:32:59 +0530

    function3

---
+++ function3.java
@@ -0,0 +1,13 @@
+public class function3 {
+    public static void main(String[] args) {
+        helloworld();
+    }
+
+    public static void helloworld() {
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("sowrav");
+        }
+    }
+
+}
